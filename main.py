e48b1cbcd86957b551980c48173a0d34ac0464be
claytonfollett2021 <813551@mystma.org>
2020-12-29 03:47:47 +0000

    topFiveMovies(1)

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(wordOne, wordTwo, wordThree, wordFour, wordfive):
+    return wordOne + wordTwo + wordThree + wordFour + wordfive
+
+print (topFiveMovies("WW84", "Black Panther", "Avengers", "Us", "Toy Story 4"))
